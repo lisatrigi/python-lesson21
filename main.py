3822d3306374f3d736572823461e0f0a0b7f964f
lisatrigi <lisashahini2009@gmail.com>
2025-10-16 20:23:20 +0200

    HTTPS methods - get, post, put, delete

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ def read_items():
 @app.post("/items/")
 def create_items(name:str,price:float):
     return{"item_name":name, "item_price": price}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, name: str, price: float):
+    return{"item_id":item_id, "name":name, "price":price}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return{"item_id":f"item {item_id} deleted"}
